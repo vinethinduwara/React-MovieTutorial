126d2c5c6fcdc48ff9c41afea757b13b8154623e
vinethinduwara <viarandarage@gmail.com>
2025-06-10 10:46:18 +0530

    Update the Movie Card and Implement the navbar

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -18,7 +18,7 @@ function MovieCard({ movie }) {
       </div>
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
+        <p>{movie.release_date?.split("-")[0]}</p>
       </div>
     </div>
   );
