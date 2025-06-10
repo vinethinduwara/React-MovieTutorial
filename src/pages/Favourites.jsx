41e53de6c91145582e97519f213728de8d6f181a
vinethinduwara <viarandarage@gmail.com>
2025-06-10 14:01:52 +0530

    Implement the Favourites Component

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,12 @@
+import "../css/Favourites.css";
+
+function Favourites() {
+  return (
+    <div className="favorites-empty">
+      <h2>No Favourites yet.</h2>
+      <p>Start adding movies to your favourites</p>
+    </div>
+  );
+}
+
+export default Favourites;
